a1e0de4a471f88d6d1872a4b6d78da050cfd7a25
Ivan Tsiupa <vattgert@gmail.com>
2025-02-27 22:25:26 +0200

    implement player's camera following boundaries

--- Assets/Test Club/Scripts/Player/PlayerCameraFollow.cs
+++ Assets/Test Club/Scripts/Player/PlayerCameraFollow.cs
@@ -1,13 +1,22 @@
+using System;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class CameraFollow : MonoBehaviour
 {
+    [SerializeField]
+    private float borderMarginX;
+    [SerializeField]
+    private float borderMarginY;
     public Transform player;
     public Vector3 offset;
+    public Rigidbody2D map;
+    private CompositeCollider2D mapCollider;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         offset = new Vector3(0, 0, -1);   
+        mapCollider = map.GetComponent<CompositeCollider2D>();
     }
 
     // Update is called once per frame
@@ -16,12 +25,34 @@ public class CameraFollow : MonoBehaviour
         
     }
 
+    private Vector3 calculateCameraPosition(CompositeCollider2D mapCollider)
+    {
+        Vector3 cameraPosition = player.position + offset;
+        if (mapCollider != null)
+        {
+            Bounds mapBounds = mapCollider.bounds;
+
+            // Calculate clamped x position:
+            // When the desired camera x is greater than the right limit, we clamp it.
+            // When it's less than the left limit, we clamp it too.
+            float minX = mapBounds.min.x + borderMarginX;
+            float maxX = mapBounds.max.x - borderMarginX;
+
+            float minY = mapBounds.min.y - borderMarginY;
+            float maxY = mapBounds.max.y + borderMarginY;
+
+            cameraPosition.x = Mathf.Clamp(cameraPosition.x, minX, maxX);
+            cameraPosition.y = Mathf.Clamp(cameraPosition.y, minY, maxY);
+        }
+        return cameraPosition;
+
+    }
+
     private void LateUpdate()
     {
         if (player != null)
         {
-            // Update the camera's position to follow the player's position with the offset
-            transform.position = player.position + offset;
+            transform.position = calculateCameraPosition(this.mapCollider);
         }
     }
 }
